638e042f3326c644c305d8f79898d8d60877a8d8
AMoraby3 <01205082343ahmed@gmail.com>
2022-12-10 13:24:48 +0200

    merge map

--- src/app/dash-board/component/sned-requests/sned-requests.component.ts
+++ src/app/dash-board/component/sned-requests/sned-requests.component.ts
@@ -23,31 +23,48 @@ export class SnedRequestsComponent implements OnInit {
     this._CustomerTicketsService.getCustomsClearance().subscribe((res:any) => {
       this.data1 = res.data
       //console.log(this.data1);
-      this.all.push(this.data1 , this.data2 ,this.data3 ,this.data4)
-      console.log(this.all);
-      for (let index = 0; index < 4; index++) {
-        for (let i = 0; i < this.all[i].length ; i++) {
-          //console.log(this.all[i].length);
-          console.log(this.all[i]);
+          for (let i = 0; i < this.data1.length ; i++) {
+              console.log(this.data1[i].supplier_id);
 
-        }
-     }
+            }
+
     })
     this.airfreigft.getAirFreight().subscribe((res:any) => {
       this.data2 = res.data;
       //console.log(this.data2);
+      for (let i = 0; i < this.data2.length ; i++) {
+        console.log(this.data2[i].supplier_id);
+
+      }
     })
     this.seafreight.getSeaFreight().subscribe((res:any) => {
       this.data3 = res.data
-      //console.log(this.data3);
+     // console.log(this.data3);
+      for (let i = 0; i < this.data3.length ; i++) {
+        console.log(this.data3[i].supplier_id);
+
+      }
 
     })
     this.localfreight.getLocalFreight().subscribe((res:any) => {
       this.data4 = res.data
-      //console.log(this.data4);
+     // console.log(this.data4);
+      for (let i = 0; i < this.data4.length ; i++) {
+        console.log(this.data4[i].supplier_id);
+
+      }
     })
 
 
   }
 
 }
+ //   for (let index = 0; index < 4; index++) {
+    //     for (let i = 0; i < this.all[i].length ; i++) {
+    //       //console.log(this.all[i].length);
+    //       console.log(this.all[i]);
+
+    //     }
+    //  }
+    // this.all.push(this.data1 , this.data2 ,this.data3 ,this.data4)
+    // console.log(this.all);
